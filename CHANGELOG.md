2ac040c582f67ce3adf605a868b16cbca5e3262c
Samuel Birch <sam.birch@gmail.com>
2023-09-19 14:09:42 +0100

    checkDateInRange

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## 2.4.0 - 2023-09-19
+### Added
+- checkDateInRange function
+- Last Week & Last Month date options
+### Changed
+- optimised variable methods
+
 ## 2.3.3 - 2023-04-21
 ### Fixed
 - all starts have time set to 0
--- src/variables/Reports.php
+++ src/variables/Reports.php
@@ -23,81 +23,15 @@ class Reports
     public function configure(mixed $query, string $attribute, array $options): mixed
     {
         $criteria = [];
-        $startDate = null;
-        $endDate = null;
         
-        $today = DateTimeHelper::toDateTime(strtotime('today'));
-        $today->setTime(0,0,0);
+        $range = $this->_dateRange($options);
         
-        switch ($options['dateRange']['type'])
-        {
-            case 'Custom':
-                if (!empty($options['dateRange']['startDate'])) {
-                    $startDate = DateTimeHelper::toDateTime($options['dateRange']['startDate'])->setTime(0,0,0);
-                }
-                if (!empty($options['dateRange']['endDate'])) {
-                    $endDate = DateTimeHelper::toDateTime($options['dateRange']['endDate']);
-                    $endDate->setTime(0,0,0)->add(new DateInterval('P1D'));
-                }
-                break;
-                
-            case 'Today':
-                $startDate = $today;
-                $endDate = (clone $startDate)->add(new DateInterval('P1D'));
-                break;
-                
-            case 'Yesterday':
-                $startDate = (clone $today)->sub(new DateInterval('P1D'));
-                $endDate = $today;
-                break;
-                
-            case 'This Week':
-                $startDate = DateTimeHelper::toDateTime(strtotime('last monday', strtotime('tomorrow')))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1W'));
-                break;
-            
-            case 'This Month':
-                $startDate = DateTimeHelper::toDateTime(strtotime('first day of this month'))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1M'));
-                break;
-                
-            case 'This Year':
-                $startDate = DateTimeHelper::toDateTime(strtotime('first day of January'))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1Y'));
-                break;
-                
-            case 'Past 7 Days':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P7D'));
-                break;
-                
-            case 'Past 30 Days':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P30D'));
-                break;
-                
-            case 'Past 90 Days':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P90D'));
-                break;
-                
-            case 'Past Year':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P1Y'));
-                break;
-                
-            case 'This Financial Year':
-                $startDate = DateTimeHelper::toDateTime(strtotime('first day of April'))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1Y'));
-                break;
-        }
-        
-        if ($startDate && $endDate) {
-            $criteria[$attribute] = ['and', '>= '.DateTimeHelper::toIso8601($startDate), '< '.DateTimeHelper::toIso8601($endDate)];
-        } elseif ($startDate) {
-            $criteria[$attribute] = '>= '.DateTimeHelper::toIso8601($startDate);
-        } elseif ($endDate) {
-            $criteria[$attribute] = '< '.DateTimeHelper::toIso8601($endDate);
+        if ($range['start'] && $range['end']) {
+            $criteria[$attribute] = ['and', '>= '.DateTimeHelper::toIso8601($range['start']), '< '.DateTimeHelper::toIso8601($range['end'])];
+        } elseif ($range['start']) {
+            $criteria[$attribute] = '>= '.DateTimeHelper::toIso8601($range['start']);
+        } elseif ($range['end']) {
+            $criteria[$attribute] = '< '.DateTimeHelper::toIso8601($range['end']);
         }
         
         Craft::configure($query, $criteria);
@@ -107,81 +41,14 @@ class Reports
     
     public function getDateRange(string $attribute, array $options): mixed
     {
-        $startDate = null;
-        $endDate = null;
+        $range = $this->_dateRange($options);
         
-        $today = DateTimeHelper::toDateTime(strtotime('today'));
-        $today->setTime(0,0,0);
-        
-        switch ($options['dateRange']['type'])
-        {
-            case 'Custom':
-                if (!empty($options['dateRange']['startDate'])) {
-                    $startDate = DateTimeHelper::toDateTime($options['dateRange']['startDate'])->setTime(0,0,0);
-                }
-                if (!empty($options['dateRange']['endDate'])) {
-                    $endDate = DateTimeHelper::toDateTime($options['dateRange']['endDate']);
-                    $endDate->setTime(0,0,0)->add(new DateInterval('P1D'));
-                }
-                break;
-                
-            case 'Today':
-                $startDate = $today;
-                $endDate = (clone $startDate)->add(new DateInterval('P1D'));
-                break;
-                
-            case 'Yesterday':
-                $startDate = (clone $today)->sub(new DateInterval('P1D'));
-                $endDate = $today;
-                break;
-                
-            case 'This Week':
-                $startDate = DateTimeHelper::toDateTime(strtotime('last monday', strtotime('tomorrow')))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1W'));
-                break;
-            
-            case 'This Month':
-                $startDate = DateTimeHelper::toDateTime(strtotime('first day of this month'))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1M'));
-                break;
-                
-            case 'This Year':
-                $startDate = DateTimeHelper::toDateTime(strtotime('first day of January'))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1Y'));
-                break;
-                
-            case 'Past 7 Days':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P7D'));
-                break;
-                
-            case 'Past 30 Days':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P30D'));
-                break;
-                
-            case 'Past 90 Days':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P90D'));
-                break;
-                
-            case 'Past Year':
-                $endDate = $today;
-                $startDate = (clone $endDate)->sub(new DateInterval('P1Y'));
-                break;
-                
-            case 'This Financial Year':
-                $startDate = DateTimeHelper::toDateTime(strtotime('first day of April'))->setTime(0,0,0);
-                $endDate = (clone $startDate)->add(new DateInterval('P1Y'));
-                break;
-        }
-        
-        if ($startDate && $endDate) {
-            return ['and', $attribute.' >= "'.$startDate->format('Y-m-d\T00:00:00').'"', $attribute.' < "'.$endDate->format('Y-m-d\T00:00:00').'"'];
-        } elseif ($startDate) {
-            return $attribute.' >= "'.$startDate->format('Y-m-d\T00:00:00').'"';
-        } elseif ($endDate) {
-            return $attribute.' < "'.$endDate->format('Y-m-d\T00:00:00').'"';
+        if ($range['start'] && $range['end']) {
+            return ['and', $attribute.' >= "'.$range['start']->format('Y-m-d\T00:00:00').'"', $attribute.' < "'.$range['end']->format('Y-m-d\T00:00:00').'"'];
+        } elseif ($range['start']) {
+            return $attribute.' >= "'.$range['start']->format('Y-m-d\T00:00:00').'"';
+        } elseif ($range['end']) {
+            return $attribute.' < "'.$range['end']->format('Y-m-d\T00:00:00').'"';
         }
         
         return null;
@@ -199,5 +66,107 @@ class Reports
             
             Craft::$app->db->createCommand('UPDATE '.$table.' SET `data` = CONCAT(data, '.$data.') WHERE id = '.$id)->execute();
         }
+    }
+    
+    public function checkDateInRange($date, array $options): bool
+    {
+        $range = $this->_dateRange($options);
+        
+        $date = DateTimeHelper::toDateTime($date);
+            
+        if ($range['start'] && $range['end']) {
+            return $date >= $range['start'] && $date < $range['end'];
+        } elseif ($range['start']) {
+            return $date >= $range['start'];
+        } elseif ($range['end']) {
+            return $date < $range['end'];
+        }
+        
+        return false;
+    }
+    
+    
+    private function _dateRange(array $options): array
+    {
+        $startDate = null;
+        $endDate = null;
+        
+        $today = DateTimeHelper::toDateTime(strtotime('today'));
+        $today->setTime(0,0,0);
+        
+        switch ($options['dateRange']['type'])
+        {
+            case 'Custom':
+                if (!empty($options['dateRange']['startDate'])) {
+                    $startDate = DateTimeHelper::toDateTime($options['dateRange']['startDate'])->setTime(0,0,0);
+                }
+                if (!empty($options['dateRange']['endDate'])) {
+                    $endDate = DateTimeHelper::toDateTime($options['dateRange']['endDate']);
+                    $endDate->setTime(0,0,0)->add(new DateInterval('P1D'));
+                }
+                break;
+                
+            case 'Today':
+                $startDate = $today;
+                $endDate = (clone $startDate)->add(new DateInterval('P1D'));
+                break;
+                
+            case 'Yesterday':
+                $startDate = (clone $today)->sub(new DateInterval('P1D'));
+                $endDate = $today;
+                break;
+                
+            case 'This Week':
+                $startDate = DateTimeHelper::toDateTime(strtotime('last monday', strtotime('tomorrow')))->setTime(0,0,0);
+                $endDate = (clone $startDate)->add(new DateInterval('P1W'));
+                break;
+                
+            case 'Last Week':
+                $startDate = DateTimeHelper::toDateTime(strtotime('last monday', strtotime('tomorrow')))->setTime(0,0,0)->sub(new DateInterval('P1W'));
+                $endDate = (clone $startDate)->add(new DateInterval('P1W'));
+                break;
+            
+            case 'This Month':
+                $startDate = DateTimeHelper::toDateTime(strtotime('first day of this month'))->setTime(0,0,0);
+                $endDate = (clone $startDate)->add(new DateInterval('P1M'));
+                break;
+                
+            case 'Last Month':
+                $startDate = (DateTimeHelper::toDateTime(strtotime('first day of last month'))->setTime(0,0,0));
+                $endDate = (clone $startDate)->add(new DateInterval('P1M'));
+                break;
+                
+            case 'This Year':
+                $startDate = DateTimeHelper::toDateTime(strtotime('first day of January'))->setTime(0,0,0);
+                $endDate = (clone $startDate)->add(new DateInterval('P1Y'));
+                break;
+                
+            case 'Past 7 Days':
+                $endDate = $today;
+                $startDate = (clone $endDate)->sub(new DateInterval('P7D'));
+                break;
+                
+            case 'Past 30 Days':
+                $endDate = $today;
+                $startDate = (clone $endDate)->sub(new DateInterval('P30D'));
+                break;
+                
+            case 'Past 90 Days':
+                $endDate = $today;
+                $startDate = (clone $endDate)->sub(new DateInterval('P90D'));
+                break;
+                
+            case 'Past Year':
+                $endDate = $today;
+                $startDate = (clone $endDate)->sub(new DateInterval('P1Y'));
+                break;
+                
+            case 'This Financial Year':
+                $startDate = DateTimeHelper::toDateTime(strtotime('first day of April'))->setTime(0,0,0);
+                $endDate = (clone $startDate)->add(new DateInterval('P1Y'));
+                break;
+        }
+        
+        return ['start' => $startDate, 'end' => $endDate];
     }
 }
